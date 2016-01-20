643f5b81d874c675b7fcf344044658c4a80bc216
Brianna Popson and Christine Tacha <bpopson@gmail.com>
2016-01-20 11:05:52 -0800

    add spec to determine if first letter of word is a vowel and add ay

--- pigLatin/js/scripts.js
+++ pigLatin/js/scripts.js
@@ -1,7 +1,13 @@
+var ay = "ay"
+
 var pigLatin = function(word) {
   var firstLetter = word.charAt(0);
-  word = word.substring(1);
-  word = word + firstLetter;
-  var newWord = word.concat("ay");
-  return newWord;
+    if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u"){
+    return word + ay;
+  } else {
+    word = word.substring(1);
+    word = word + firstLetter;
+    var newWord = word.concat(ay);
+    return newWord;
+  }
 };
--- pigLatin/specs/specs.js
+++ pigLatin/specs/specs.js
@@ -1,8 +1,10 @@
 describe('pigLatin', function() {
+
   it("will remove the first letter of a word and place it at the end of the word and add 'ay'", function() {
     expect(pigLatin("Pig")).to.equal("igPay");
   });
-//   it("will add 'ay' to the end of a word", function() {
-//     expect(pigLatin("Pig")).to.equal("igPay");
-//   });
+
+  it("will determine whether a word begins with a vowel or not", function() {
+    expect(pigLatin("unicorn")).to.equal("unicornay");
+  });
 });
