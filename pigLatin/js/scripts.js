6bd7caccfd8617f758074c25d91aa0a0b46b1df1
Brianna Popson and Christine Tacha <tacha.christine@gmail.com>
2016-01-20 14:21:49 -0800

    add a semi-working loop for adding ay to vowel word and advice on how to proceed

--- pigLatin/js/scripts.js
+++ pigLatin/js/scripts.js
@@ -1,24 +1,46 @@
-// var ay = "ay"
-// var pigLatin = function(word) {
-//   var firstLetter = word.charAt(0);
-//     if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u"){
-//     return word + ay;
-//   } else {
-//     word = word.substring(1);
-//     word = word + firstLetter;
-//     var newWord = word.concat(ay);
-//     return newWord;
-//   }
-// };
+var ay = "ay"
+var pigLatin = function(word) {
+  var firstLetter = word.charAt(0);
+    if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u"){
+    return word + ay;
+  } else {
+    word = word.substring(1);
+    word = word + firstLetter;
+    var newWord = word.concat(ay);
+    return newWord;
+  }
+};
 var pigLoop = function(word) {
-  var word = "steal"
+  var word = "steal";
   var ay = "ay";
+  var newWord;
+
     for (i = 0; i < word.length; i ++){
-        var firstLetter = word.charAt(0);
-        if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u"){
-        word = word + ay;
+        var firstLetter = word.charAt(i);
+        if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {break;} {
+          newWord = word + ay;
+        }
+      return newWord;
+    }
+  }
+
+          //stop when vowel is reached
+          //slice off preceeding cons and save in new variable
+          //concat new variable to word
+          //concat ay
+
+
+          newWord = word + ay;
+        }
+      }
+      return newWord;
+    }
+
+
+
+
       } else {
-        newWord = word.substring(0);
+        newWord = word.substring(i);
         finalWord = newWord + firstLetter;
       }
     }
